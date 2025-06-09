6d05b47a6e3a70597b2cb5fa67b7de57d6e747de
vitu1415 <vitoralves0801@gmail.com>
2025-06-09 00:25:40 -0300

    retirada da validacao mocada - rotina

--- src/Utils/ValidadorDeAtrasos.ts
+++ src/Utils/ValidadorDeAtrasos.ts
@@ -4,12 +4,12 @@ import { UsuarioService } from "../service/UsuarioService";
 export function iniciarVerificacaoDeAtrasos() {
     const emprestimoService = new EmmprestimoService();
     const usuarioService = new UsuarioService();
-    const intervaloEmMilissegundos = 60 * 60 * 1000;
+    const intervaloUmaHora = 60 * 60 * 1000;
 
   setInterval(() => {
     console.log('Rodando verificação de atrasos...');
     emprestimoService.verificadorDeAtraso();
     console.log('rodando verificação de usuários suspensos...');
     usuarioService.reativarUsuariosSuspensos();
-  }, 10000);
+  }, intervaloUmaHora);
 }
